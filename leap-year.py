8f81e6fc97a4e4e5f074f3bcd64b548340208a93
Özge BAYRAM <ozgebayram1234@gmail.com>
2022-07-26 20:15:05 +0300

    Update leap-year.py

--- leap-year.py
+++ leap-year.py
@@ -4,12 +4,12 @@ def CheckLeap(Year):
   if((Year % 400 == 0) or  # sorgu yapıldı
      (Year % 100 != 0) and  
      (Year % 4 == 0)):   
-    print("Given Year is a leap Year Cooper");  
+    print("Given Year is a leap Year ");  
 
   # Else it is not a leap year  
   else:  
-    print ("Given Year is not a leap Year Cooper")  
+    print ("Given Year is not a leap Year ")  
 # Taking an input year from user  
 Year = int(input("Enter the number: "))  
 # Printing result  
-CheckLeap(Year) i 
+CheckLeap(Year) 
